be2f0438325b0ecf72c3a0529552824eb102aa82
Reza <52901424+al-Reza89@users.noreply.github.com>
2023-11-26 07:40:06 +0600

    Metadata (#3)
    * add metadata in every page
    
    * dynamic metadata with id

--- app/posts/[id]/page.tsx
+++ app/posts/[id]/page.tsx
@@ -1,3 +1,4 @@
+import type { Metadata } from "next";
 import React from "react";
 
 const PostPage = () => {
@@ -5,3 +6,24 @@ const PostPage = () => {
 };
 
 export default PostPage;
+
+type Props = {
+  params: { id: string };
+  searchParams: { [key: string]: string | string[] | undefined | null };
+};
+
+export async function generateMetadata({
+  params,
+  searchParams,
+}: Props): Promise<Metadata> {
+  const id = params.id;
+
+  const product = await fetch(`https://dummyjson.com/posts/${params.id}`).then(
+    (res) => res.json()
+  );
+
+  return {
+    title: product.title,
+    description: "this is individual product desc",
+  };
+}
--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import React from "react";
+
+export const metadata: Metadata = {
+  title: "Create your post",
+  description: "you can see all your post",
+};
 
 const Posts = () => {
   return <div>Posts</div>;
