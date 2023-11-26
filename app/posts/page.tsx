84a4f8ff1ef30b0b84dfcd5320af850a11eb1f8a
al-Reza89 <alialreza98@gmail.com>
2023-11-26 09:32:34 +0600

    remove unnecessary seed file

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -14,6 +14,7 @@ export const metadata: Metadata = {
 const Posts = async () => {
   try {
     const posts = await prisma.post.findMany();
+
     return (
       <main className="text-center pt-16 px-5">
         <h1 className="text-5xl font-semibold mb-7">All posts</h1>
