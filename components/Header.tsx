2b9179498f28c701a06da6899752f0961e16b33b
al-Reza89 <alialreza98@gmail.com>
2023-11-26 06:54:07 +0600

    add metadata in every page

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,7 +8,6 @@ const Header = () => {
   return (
     <header className="sticky top-0 z-20 backdrop-blur-lg  py-4 px-7 border-b  ">
       <div className="max-w-[1250px] mx-auto flex justify-between items-center ">
-
         <Link href={"/"}>
           <Image
             src="https://placehold.jp/000000/ffffff/150x150.png?text=B&css=%7B%22border-radius%22%3A%2215px%22%7D"
